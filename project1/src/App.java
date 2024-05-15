ff1bc2182d2482381c978973e89176119d9432b9
milenasvarela <milenadesouzavarela@gmail.com>
2024-05-14 21:17:31 -0300

    1º commit

---
+++ project1/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+
+    public class
+    void main
+    {String [] args){
+        
+        //Declaração das variáveis
+
+        String sMsg1 ="Tecnico Informatica";
+        String sMsg2 ="Semestre";
+        int iValor1 = 2; //%d
+        int iValor2 = 1; //%d
+        float fValor1 = 58.700001f; //%f
+        char cValor1 = '%'; //%c
+        boolean bExcelencia = true; //%b
+        String sMsg3 = "Duvidas??";
+        String sMsg4 = "Por favor chamem!";
+        String sMsg5 = "Muito obrigada pela sua atenção!";
+        String sMsg6 = String.format("%s. \n %s. \n \n %s", sMsg3, sMsg4, sMsg5);
+
+
+        
+    
+    }
+    }
+}
