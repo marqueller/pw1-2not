b2b429d1d9d6cff3de9b5601a9692a7e01a288e9
Ernani <4553027258@estudante.sed.sc.gov.br>
2024-05-14 21:47:16 -0300

    1° commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        // Declaração de variáveis:
+        String sMsg1 = "Tecnico Informatica";
+        String sMsg2 = "Semestre";
+        int iValor1 = 2; // %d
+        int iValor2 = 1; // %d
+        float fValor1 = 58.700001f; // %f
+        char cValor = '%'; // c
+        boolean Excelencia = true; // %b
+
+        // Usando multiplas Strings:
+        // Declarando:
+        String sMsg3 = "Duvidas??? ";
+
+    }
+}
