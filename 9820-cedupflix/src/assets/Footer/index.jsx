2952bb56281ace5bb2270196971beb672490ae57
luis <monteirosales51@gmail>
2024-05-28 21:32:28 -0300

    1° commit

---
+++ 9820-cedupflix/src/assets/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <Footer className={styles.footer}>
+            <h2>CedupFlix &copy; Desenvolvido por "luis" 2024</h2>
+        </Footer>
+    );
+}
+export default Footer;
