3bd06921f60a9c93dcdc2e06ca41f7a444326de4
Ernani <4553027258@estudante.sed.sc.gov.br>
2024-06-03 20:16:03 -0300

    1° commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Olá mundo!!!</h1>
+      <p>olá Mundão! Estou aprendendo React JS!</p>
+      <Footer />
+    </>
+  )
+}
