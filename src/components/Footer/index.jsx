aa75b1be28dbaeddc677d7819ecfdff810590665
Alisson <ninjalendario012@gmail.com>
2024-06-03 20:10:38 -0300

    2º commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <h2>CedupFlix &copy; Desenvolvido por Alisson 2024</h2>
+            </footer>
+        </div>
+    );
+}
