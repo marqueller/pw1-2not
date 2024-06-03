c56bf297392d2872829d47c3ac7ab1e81b5548a5
M0rgss <4553030623@estudante.sed.sc.gov.br>
2024-06-03 20:35:54 -0300

    2° commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Heaer from './components/Heaer'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <header />
+      <h1> OLÁ MUNDO!!!</h1>
+      <p>olá mundão ! estou aprendendo React JS!</p>
+    </>
+  )
+}
