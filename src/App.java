a89724769d6b865b457eca5eeffefce649ac28a5
GabrielPS2001 <160795462+GabrielPS2001@users.noreply.github.com>
2024-06-13 16:24:35 -0300

    2º commit

--- src/App.java
+++ src/App.java
@@ -1,20 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //Declaração das variáveis
-
-        String sMsg1 ="Tecnico Informatica";
-        String sMsg2 ="Semestre";
-        int iValor1 = 2; //%d
-        int iValor2 = 1; //%d
-        float fValor1 = 58.700001f; //%f
-        char cValor1 = '%'; //%c
-        boolean bExcelencia = true; //%b
-        String sMsg3 = "Duvidas??";
-        String sMsg4 = "Por favor chamem!";
-        String sMsg5 = "Muito obrigada pela sua atenção!";
-        String sMsg6 = String.format("%s. \n %s. \n \n %s", sMsg3, sMsg4, sMsg5);
-
         
+
+        String sMsg1 = "Técnico Informática";
+        String sMsg2 = "Semestre";
+        int iValor1 = 2; // %d
+        int iValor2 = 1; // %d
+        float fValor1 = 58.700001f; // %f
+        char cValor1 = '%'; // %c
+        boolean bExcelência = true; // %b
+
+        String sMsg3 = "Duvidas???";
+        String sMsg4 = "Por favor chamem!!";
+        String sMsg5 = "Muito obrigado pela atenção";
+        String sMsg6 = String.format("%s \n %s.\n \n %s", sMsg3, sMsg4, sMsg5);
+
+        // Saída de dados para o usuário:
+        System.out.println(sMsg6);
 
         
         
