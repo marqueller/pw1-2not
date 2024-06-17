3ac62c03418a0e7a92768d6fd159c8abca89018b
Jean5698 <4553027347@estudante.sed.sc.gov.br>
2024-06-17 20:23:29 -0300

    3° commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
+import './App.css'
+
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Olá Mundo!!!</h1>
-      <p>Olá mundão! Estou aprendendo React JS!</p>
       <Footer />
     </>
   )
 }
+
+export default App
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <h2>CedupFlix &copy; Desenvolvido por Jean 2024</h2>
+            </footer>
+        </div>
+    );
+}
