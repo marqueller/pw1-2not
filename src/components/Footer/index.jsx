c9bdbc4d78b5f917731c2a127e7d301f4b67a3c7
Ernani <4553027258@estudante.sed.sc.gov.br>
2024-06-18 18:49:43 -0300

    3° commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <h2>CedupFlix &copy; Desenvolvido por Ernani</h2>
+            </footer>
+        </div>
+    );
+}
